db2821d8607d0b5df075597c15fe5c0ec372d880
interesta22 <2albelasd@gmail.com>
2025-10-21 09:39:54 +0300

    Finish ( create account - email sent - forgot password - bottom nav bar )

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:depi_final_project/features/store/screens/home.dart';
 import 'package:depi_final_project/features/layout/BottomNavLayout.dart';
 import 'package:depi_final_project/features/store/screens/search_page.dart';
+import 'package:depi_final_project/features/authentication/screens/email_sent.dart';
 import 'package:depi_final_project/features/authentication/screens/signin_email.dart';
 import 'package:depi_final_project/features/authentication/screens/splash_screen.dart';
+import 'package:depi_final_project/features/authentication/screens/create_account.dart';
+import 'package:depi_final_project/features/authentication/screens/forgot_password.dart';
 import 'package:depi_final_project/features/authentication/screens/signin_password.dart';
 
 class AppRoutes {
@@ -19,6 +22,7 @@ class AppRoutes {
   static const String sentEmail = '/sentEmail';
   static const String tellUsAboutYou = '/tellUsAboutYou';
   static const String home = '/home';
+  static const String emailSent = '/emailSent';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -33,9 +37,13 @@ class AppRoutes {
       case signInPassword:
         return MaterialPageRoute(builder: (_) => SignInPassword());
       case signUp:
-        return MaterialPageRoute(builder: (_) => SignInEmail());
+        return MaterialPageRoute(builder: (_) => CreateAccount());
       case home:
-        return MaterialPageRoute(builder: (_) => const HomeView());
+        return MaterialPageRoute(builder: (_) => const BottomNavLayout());
+      case forgetPassword:
+        return MaterialPageRoute(builder: (_) => const ForgotPassword());
+      case emailSent:
+        return MaterialPageRoute(builder: (_) => const EmailSent());
       default:
         return _errorRoute("Route not found: ${settings.name}");
     }
---
+++ lib/features/layout/CustomBottomBar.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
+import 'package:depi_final_project/core/theme/colors.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+
+class CustomBottomBar extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  const CustomBottomBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final items = [
+      SalomonBottomBarItem(
+        icon: HugeIcon(
+          icon: HugeIcons.strokeRoundedHome03,
+          color: AppColors.darkPrimary,
+          size: 20,
+        ),
+        title: Text('Home'),
+        selectedColor: AppColors.darkPrimary,
+      ),
+      SalomonBottomBarItem(
+        icon: HugeIcon(
+          icon: HugeIcons.strokeRoundedReceiptDollar,
+          color: AppColors.darkPrimary,
+          size: 20,
+        ),
+        title: Text('Orders'),
+        selectedColor: AppColors.darkPrimary,
+      ),
+      SalomonBottomBarItem(
+        icon: HugeIcon(
+          icon: HugeIcons.strokeRoundedHotelBell,
+          color: AppColors.darkPrimary,
+          size: 20,
+        ),
+        title: Text('Notifications'),
+        selectedColor: AppColors.darkPrimary,
+      ),
+
+      SalomonBottomBarItem(
+        icon: HugeIcon(
+          icon: HugeIcons.strokeRoundedSetting06,
+          color: AppColors.darkPrimary,
+          size: 20,
+        ),
+        title: Text('Settings'),
+        selectedColor: AppColors.darkPrimary,
+      ),
+    ];
+
+    return SalomonBottomBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      items: items,
+    );
+  }
+}
